dee872a39d2db17c01d716152c89e4028c8d17fe
Santiago Faci <santiago.faci@gmail.com>
2018-02-05 10:47:48 +0100

    First try. You can create and upload a backup directory to a FTP server

--- abbackup.py
+++ abbackup.py
@@ -1,20 +1,46 @@
 #!/usr/bin/python3
+import configparser
+import argparse
+import sys
+import shutil
+import os
 
 from ftplib import FTP
-import configparser
+from datetime import date
 
-config = configparser.ConfigParser()
-config.sections()
-config.read('abackup.conf')
+CWD = os.getcwd()
+parser = argparse.ArgumentParser()
+parser.add_argument('--directory-name', help='Directory to back up', metavar='FILENAME')
 
-host = config['ftp_server']['host']
-port = config['ftp_server']['port']
-username = config['ftp_server']['username']
-password = config['ftp_server']['password']
+print("\nabbackup 0.0.1: A backup tool (http://www.github.com/arkabytes/abbackup)")
+args = parser.parse_args()
 
-ftp = FTP(host, username, password)
-#ftp.login()
-print('login ok')
-ftp.retrlines('LIST')
-ftp.quit()
-print('quit ok')
+if len(sys.argv) == 1:
+    print("No arguments provided. Execute '" + sys.argv[0] + " -h' for help")
+    exit()
+
+if args.directory_name:
+
+    backup_filename = args.directory_name + '_' + str(date.today())
+    # Create zip file from directory
+    shutil.make_archive(backup_filename, 'zip', args.directory_name)
+
+    # Reading config file to connect to FTP server
+    config = configparser.ConfigParser()
+    config.sections()
+    config.read('abbackup.conf')
+
+    # Reading config information about ftp connection
+    host = config['ftp_server']['host']
+    port = config['ftp_server']['port']
+    username = config['ftp_server']['username']
+    password = config['ftp_server']['password']
+
+    # Connect to ftp server and upload backup file
+    ftp = FTP(host, username, password)
+    print('Login ok')
+    print('Uploading file . . .')
+    ftp.storbinary('STOR ' + backup_filename + '.zip', open(backup_filename + '.zip', 'rb'))
+    print("Quitting . . .")
+    ftp.quit()
+    print('Bye')
